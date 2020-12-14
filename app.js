62e49da19ec0924ba032eb140dea503d0c7557c8
Johnny Chang <zjps3407@gmail.com>
2020-12-14 11:33:28 +0800

    express server set up andview template engine set up

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+// view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// middleware
+app.use(express.urlencoded({ extended: true }))
+
+
+// routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => console.log(`Express server is on port: ${port}`))
